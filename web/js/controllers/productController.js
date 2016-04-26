724829b795cad3b55cd0a1bf61e9daa9bdbf8350
Efrain Aperador <efrain.aperador@gmail.com>
2016-04-25 21:50:04 -0500

    Add FE development

--- web/js/controllers/productController.js
+++ web/js/controllers/productController.js
@@ -2,7 +2,26 @@ fmcomputadores.controller('productCtrl', ['$scope', '$http', '$location', functi
 	var id = $location.search().id;
 	$http.get('/tienda-1.0/webresources/producto/' + id).then(function(response){
 		$scope.product = response.data;
+		$scope.product.especificaciones = $scope.product.especificacion.split(/\n/g);
+		$('.carousel').lightSlider({
+	        gallery:true,
+	        item:1,
+	        loop:true,
+	        thumbItem:9,
+	        slideMargin:0,
+	        enableDrag: false,
+	        currentPagerPosition:'left',
+	        onSliderLoad: function(el) {
+	            el.lightGallery({
+	                selector: '.carousel .lslide'
+	            });
+	        }   
+	    }); 
 	}, function(error){
 
 	});
+
+	$scope.changeImage = function(){
+
+	}
 }]);
